6248f1f4393a7e10ca681680d17a53d7f1e075c8
Omkar Pawar <98583817+omipawar@users.noreply.github.com>
2026-04-03 19:16:40 +0530

    Create install_terraform.sh

---
+++ install_terraform.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing Terraform..."
+
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+
+echo "Terraform installed:"
+terraform -version
