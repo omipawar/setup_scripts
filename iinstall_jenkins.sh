0065589a13794a60f7c83f25669ed149c02f87c8
Omkar Pawar <98583817+omipawar@users.noreply.github.com>
2026-04-13 19:40:37 +0530

    Create iinstall_jenkins.sh

---
+++ iinstall_jenkins.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e #exit on error
+
+echo "Installing jenkins..."
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
+
+echo "Jenkins installed:"
+
+sudo systemctl enable jenkins
+
+sudo systemctl start jenkins
+
+sudo systemctl status jenkins
+
+echo "Jenkins enabled and started successfully!"
